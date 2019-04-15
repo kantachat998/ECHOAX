6606176038c46b20e6401414553f8af474bdede8
kantachat998 <48541621+kantachat998@users.noreply.github.com>
2019-04-15 21:47:12 +0700

    Add files via upload

--- bgjs.js
+++ bgjs.js
@@ -15,3 +15,20 @@ $("input:text").blur(function(e) {
         $('.topic').removeClass('hide-item');
     });
 })
+
+function opentab(evt, status) {
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(status).style.display = "block";
+  evt.currentTarget.className += " active";
+}
