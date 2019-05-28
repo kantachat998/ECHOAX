615126f2fc245a9b113a86c3d6501d351a1309bb
kantachat998 <48541621+kantachat998@users.noreply.github.com>
2019-05-28 14:10:20 +0700

    OK

--- bgjs.js
+++ bgjs.js
@@ -1,20 +1,21 @@
 $(document).ready(function(){
-	$('#search').click(function(){
-		$('.nav-link').toggleClass('hide-item')
+	 $('#search').click(function(){
+	 	$('.nav-link').toggleClass('hide-item')
 		$('.search-form').toggleClass('active')
-	})
+	 })
+  
+  
+})
+$('#1').on('click', function (event){
+   $('.search-topic').removeClass('active');
+   $('.topic').removeClass('hide-item');
+ });
 
-	$('#searchbtn').click(function(e){
+$('#searchbtn').on('click', function (event){
 		$('.topic').addClass('hide-item')
     $('.search-topic').addClass('active')
-    e.stopPropagation();
-    $("1").attr("focus","focused");
-	})
-
-$("input:text").blur(function() {
-        $('.search-topic').removeClass('active');
-        $('.topic').removeClass('hide-item');
-    });
+    event.stopPropagation();
+    
 })
 
 function opentab(evt, status) {
@@ -33,23 +34,4 @@ function opentab(evt, status) {
   document.getElementById(status).style.display = "block";
   evt.currentTarget.className += " active";
 }
-$(window).resize(function () {
-  if ($(window).width() >= 1200  ) {
-    $(".btnedit1").removeClass("hidden");
-    $(".btnedit2").removeClass("active");
-  }
-  else{
-    $(".btnedit1").addClass("hidden");
-    $(".btnedit2").addClass("active");
-  }
-});
-$(window).resize(function () {
-  if ($(window).width() <= 767 &&  window.matchMedia("(orientation: portrait)").matches)  {
-    $(".ph.card.backphish.flex-wrap.flex-row").removeClass("flex-row");
-    $(".ph.card.backphish.flex-wrap").removeClass("flex-wrap");
-  }
-  else{
-    $(".ph.card.backphish").addClass("flex-wrap").addClass("flex-row");
 
-  }
-});
